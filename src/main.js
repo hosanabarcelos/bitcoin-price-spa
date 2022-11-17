a21d0f90e574dbd57d8e2412331a308ade59a012
Hosana <hosanabarceloscandido@gmail.com>
2022-11-17 18:52:13 -0300

    feat: atualiza arquivo main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import "regenerator-runtime/runtime.js";
+import './sass/global.scss';
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    el: '#app',
+    render: h => h(App)
+});
